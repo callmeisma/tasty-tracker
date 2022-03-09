4c3b9380cb4bddb4c5023e8d2851a516ba07b601
callmeisma <ismael.fdez87@gmail.com>
2022-03-08 19:46:46 -0500

    react table implementation

--- src/components/trades-list-component.js
+++ src/components/trades-list-component.js
@@ -1,43 +1,179 @@
 import React from "react";
 import uniqid from "uniqid";
+import {
+  useTable,
+  usePagination,
+  useSortBy,
+  useGlobalFilter,
+  useAsyncDebounce,
+} from "react-table";
+import { format } from "date-fns";
+import { GlobalFilter } from "./filter-global-component";
 
 const TradesList = (props) => {
+  const columns = React.useMemo(
+    () => [
+      { accessor: "account", Header: "Account" },
+      {
+        accessor: "startdate",
+        Header: "Start-Date",
+        Cell: ({ value }) => {
+          return format(new Date(value), "dd/MM/yyyy");
+        },
+      },
+      {
+        accessor: "enddate",
+        Header: "End-Date",
+        Cell: ({ value }) => {
+          return value ? format(new Date(value), "dd/MM/yyyy") : "";
+        },
+      },
+      { accessor: "symbol", Header: "Symbol" },
+      { accessor: "callput", Header: "Action" },
+      { accessor: "total", Header: "Total" },
+      { accessor: "value", Header: "Value" },
+      { accessor: "fees", Header: "Fees" },
+      { accessor: "commissions", Header: "Commissions" },
+    ],
+    []
+  );
 
+  const data = React.useMemo(() => props.trades);
+
+  const tableInstance = useTable(
+    { columns, data },
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    gotoPage,
+    pageCount,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = tableInstance;
+
+  const { globalFilter } = state;
+
+  const { pageIndex } = state;
   return (
-    <div>
-      <h3>Trades</h3>
-      <p>{props.trades.length}</p>
+    <div className="p-3">
+      <h3>Trades ({props.trades.length})</h3>
+      <div>
+        <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      </div>
+      <nav>
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>
+      </nav>
       <div className="table-responsive">
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th key="account">Account</th>
-              <th key="startdate">Start Date</th>
-              <th key="enddate">End Date</th>
-              <th key="symbol">Symbol</th>
-              <th key="action">Action</th>
-              <th key="total">Total</th>
-              <th key="value">Value</th>
-              <th key="fees">Fees</th>
-              <th key="commissions">Commissions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.trades.map(function (trade) {
-              return (
-                <tr key={uniqid()} id={trade._id}>
-                  <td key={uniqid()}>{trade.account}</td>
-                  <td key={uniqid()}>{trade.startdate.substring(0, 10)}</td>
-                  <td key={uniqid()}>{trade.enddate}</td>
-                  <td key={uniqid()}>{trade.symbol}</td>
-                  <td key={uniqid()}>{trade.callput}</td>
-                  <td key={uniqid()}>${(trade.value + trade.fees + trade.commissions).toFixed(2)}</td>
-                  <td key={uniqid()}>${trade.value}</td>
-                  <td key={uniqid()}>${trade.fees.toFixed(2)}</td>
-                  <td key={uniqid()}>${trade.commissions}</td>
+        <table
+          {...getTableProps()}
+          className="table table-hover align-middle align-items-center"
+        >
+          <thead>
+            {
+              // Loop over the header rows
+              headerGroups.map((headerGroup) => (
+                // Apply the header row props
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {
+                    // Loop over the headers in each row
+                    headerGroup.headers.map((column) => (
+                      // Apply the header cell props
+                      <th
+                        {...column.getHeaderProps(
+                          column.getSortByToggleProps()
+                        )}
+                        className="text-center"
+                      >
+                        {
+                          // Render the header
+                          column.render("Header")
+                        }
+                        <span>
+                          {column.isSorted
+                            ? column.isSortedDesc
+                              ? "Y"
+                              : "N"
+                            : ""}
+                        </span>
+                      </th>
+                    ))
+                  }
                 </tr>
-              );
-            })}
+              ))
+            }
+          </thead>
+          {/* Apply the table body props */}
+          <tbody {...getTableBodyProps()}>
+            {
+              // Loop over the table rows
+              page.map((row) => {
+                // Prepare the row for display
+                prepareRow(row);
+                return (
+                  // Apply the row props
+                  <tr {...row.getRowProps()}>
+                    {
+                      // Loop over the rows cells
+                      row.cells.map((cell) => {
+                        // Apply the cell props
+                        return (
+                          <td {...cell.getCellProps()} className="text-center">
+                            {
+                              // Render the cell contents
+                              cell.render("Cell")
+                            }
+                          </td>
+                        );
+                      })
+                    }
+                  </tr>
+                );
+              })
+            }
           </tbody>
         </table>
       </div>
--- src/components/transactions-list-component.js
+++ src/components/transactions-list-component.js
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import uniqid from "uniqid";
-import Pagination from "./pagination-component";
+import {
+  useTable,
+  usePagination,
+  useSortBy,
+  useGlobalFilter,
+  useAsyncDebounce,
+} from "react-table";
+import { format } from "date-fns";
+import { GlobalFilter } from "./filter-global-component";
 
 const TransactionsList = (props) => {
-
   const deleteTransaction = (id) => {
     axios
       .delete("http://localhost:4000/transactions/" + id)
@@ -20,105 +27,204 @@ const TransactionsList = (props) => {
       .delete("http://localhost:4000/transactions/")
       .then((res) => console.log(res.data));
 
-    const updTransactions = []
+    const updTransactions = [];
     props.setTransactions(updTransactions);
   };
 
-  const columns = [
-    { dataField: "account", text: "Account" },
-    { dataField: "date", text: "Date" },
-    { dataField: "type", text: "Type" },
-    { dataField: "action", text: "Action" },
-    { dataField: "symbol", text: "Symbol" },
-    { dataField: "instrument", text: "Instrument" },
-    { dataField: "description", text: "Description" },
-    { dataField: "value", text: "Value" },
-    { dataField: "quantity", text: "Quantity" },
-    { dataField: "avgprice", text: "Avg Price" },
-    { dataField: "commissions", text: "Commissions" },
-    { dataField: "fees", text: "Fees" },
-    { dataField: "multiplier", text: "Multiplier" },
-    { dataField: "rootsymbol", text: "Root Symbol" },
-    { dataField: "underlyingsymbol", text: "Underlying Symbol" },
-    { dataField: "expiration", text: "Expiration" },
-    { dataField: "strike", text: "Strike" },
-    { dataField: "callput", text: "C/P" },
-    { dataField: "order", text: "Order" },
-  ];
+  const columns = React.useMemo(
+    () => [
+      { accessor: "account", Header: "Account" },
+      {
+        accessor: "date",
+        Header: "Date",
+        Cell: ({ value }) => {
+          return format(new Date(value), "dd/MM/yyyy");
+        },
+      },
+      { accessor: "type", Header: "Type" },
+      { accessor: "action", Header: "Action" },
+      { accessor: "quantity", Header: "Quantity" },
+      { accessor: "underlyingsymbol", Header: "Symbol" },
+      {
+        accessor: "expiration",
+        Header: "Expiration",
+        Cell: ({ value }) => {
+          return format(new Date(value), "dd/MM/yyyy");
+        },
+      },
+      { accessor: "callput", Header: "C/P" },
+      { accessor: "strikeprice", Header: "Strike" },
+      { accessor: "avgprice", Header: "Avg" },
+      { accessor: "value", Header: "Value" },
+      { accessor: "commissions", Header: "Commissions" },
+      { accessor: "fees", Header: "Fees" },
+      { accessor: "order", Header: "Order" },
+    ],
+    []
+  );
 
-  // TODO connect pagination to transactions list
+  const data = React.useMemo(() => props.transactions);
+
+  const tableInstance = useTable(
+    { columns, data },
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    gotoPage,
+    pageCount,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = tableInstance;
+
+  const { globalFilter } = state;
+
+  const { pageIndex } = state;
 
   return (
     <div className="p-3">
-      <h3>Transactions</h3>
-      <p>{props.transactions.length}</p>
+      <h3>Transactions ({props.transactions.length})</h3>
       <div>
-      <Pagination />
-      <button onClick={() => deleteAllTransactions()}>Delete All</button>
+        <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+        <button onClick={() => deleteAllTransactions()}>Delete All</button>
+      </div>
+      <div>
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>
       </div>
       <div className="table-responsive">
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              {columns.map(function (col) {
-                return (
-                  <th id={col.dataField} key={col.dataField}>
-                    {col.text}
-                  </th>
-                );
-              })}
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.transactions.map(function (transaction) {
-              return (
-                <tr id={transaction._id} key={uniqid()}>
-                  <td key={uniqid()}>{transaction.account}</td>
-                  <td key={uniqid()}>{transaction.date.substring(0, 10)}</td>
-                  <td key={uniqid()}>{transaction.type}</td>
-                  <td key={uniqid()}>{transaction.action}</td>
-                  <td key={uniqid()}>{transaction.symbol}</td>
-                  <td key={uniqid()}>{transaction.instrument}</td>
-                  <td key={uniqid()}>{transaction.description}</td>
-                  <td key={uniqid()}>{transaction.value}</td>
-                  <td key={uniqid()}>{transaction.quantity}</td>
-                  <td key={uniqid()}>{transaction.avgprice}</td>
-                  <td key={uniqid()}>{transaction.commissions}</td>
-                  <td key={uniqid()}>{transaction.fees}</td>
-                  <td key={uniqid()}>{transaction.multiplier}</td>
-                  <td key={uniqid()}>{transaction.rootsymbol}</td>
-                  <td key={uniqid()}>{transaction.underlyingsymbol}</td>
-                  <td key={uniqid()}>{transaction.expiration}</td>
-                  <td key={uniqid()}>{transaction.strikeprice}</td>
-                  <td key={uniqid()}>{transaction.callput}</td>
-                  <td key={uniqid()}>{transaction.order}</td>
-                  <td key={uniqid()}>
-                    <div
-                      className="btn-group"
-                      role="group"
-                      aria-label="actions"
-                    >
-                      <Link
-                        className=""
-                        to={"/edit-transaction/" + transaction._id}
+        <table
+          {...getTableProps()}
+          className="table table-hover align-middle align-items-center"
+        >
+          <thead>
+            {
+              // Loop over the header rows
+              headerGroups.map((headerGroup) => (
+                // Apply the header row props
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {
+                    // Loop over the headers in each row
+                    headerGroup.headers.map((column) => (
+                      // Apply the header cell props
+                      <th
+                        {...column.getHeaderProps(
+                          column.getSortByToggleProps()
+                        )}
+                        className="text-center"
                       >
-                        <button className="btn btn-primary nav-link">
+                        {
+                          // Render the header
+                          column.render("Header")
+                        }
+                        <span>
+                          {column.isSorted
+                            ? column.isSortedDesc
+                              ? "Y"
+                              : "N"
+                            : ""}
+                        </span>
+                      </th>
+                    ))
+                  }
+                  <th>Actions</th>
+                </tr>
+              ))
+            }
+          </thead>
+          {/* Apply the table body props */}
+          <tbody {...getTableBodyProps()}>
+            {
+              // Loop over the table rows
+              page.map((row) => {
+                // Prepare the row for display
+                prepareRow(row);
+                return (
+                  // Apply the row props
+                  <tr {...row.getRowProps()} id={"test" + row.original.id}>
+                    {
+                      // Loop over the rows cells
+                      row.cells.map((cell) => {
+                        // Apply the cell props
+                        return (
+                          <td {...cell.getCellProps()} className="text-center">
+                            {
+                              // Render the cell contents
+                              cell.render("Cell")
+                            }
+                          </td>
+                        );
+                      })
+                    }
+                    <td key={uniqid()}>
+                      <div
+                        className="btn-group"
+                        role="group"
+                        aria-label="actions"
+                      >
+                        {/* <Link className="" to={"/edit-transaction/" + row._id}> */}
+                        <button
+                          className="btn btn-primary nav-link"
+                          onClick={(e) => console.log()}
+                        >
                           <i className="bi bi-pen text-light"></i>
                         </button>
-                      </Link>
-                      <button
-                        className="btn btn-danger"
-                        type="submit"
-                        onClick={(e) => deleteTransaction(transaction._id)}
-                      >
-                        <i className="bi bi-x-circle icon-white"></i>
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
+                        {/* </Link> */}
+                        <button
+                          className="btn btn-danger"
+                          type="submit"
+                          onClick={(e) => deleteTransaction(row._id)}
+                        >
+                          <i className="bi bi-x-circle icon-white"></i>
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })
+            }
           </tbody>
         </table>
       </div>
