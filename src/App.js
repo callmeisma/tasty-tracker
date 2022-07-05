f7fc0efbbc74ede132ca9f120db873d4b38b19ee
callmeisma <ismael.fdez87@gmail.com>
2022-07-04 22:02:07 -0400

    edited homepage to package.json

--- src/App.js
+++ src/App.js
@@ -134,6 +134,7 @@ function App() {
           <Route path="/" exact component={Welcome} />
           <Route path="/sign-up" exact component={SignUp} />
           <Route path="/sign-in" exact component={SignIn} />
+          <Route path="/tasty-tracker" exact component={Welcome} />
         </div>
       );
     }
